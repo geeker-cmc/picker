54aafd37a2ff3d8ac09fb6f05b0c99f1d62bef0d
caimingchao <2544403004@qq.com>
2018-06-06 19:00:22 +0800

    picker

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 !(function(win){
 
     function on(el, evt, callback){
@@ -27,7 +22,7 @@
         }
         var config = Object.assign({}, defaultConfig, config);
         this.title = config.title;
-        
+        this.height = config.itemHeight || 40;
         this._renderPop()
         this._event()
     }   
@@ -39,27 +34,28 @@
              this.cover.className = "cover"
              this.picker.className = "picker"
              this.picker.innerHTML = '<div class="header">\
-                                 <span class="cancel">取消</span>\
-                                 <span class="title">'+this.title+'</span>\
-                                 <span class="confirm">确认</span>\
-                                </div><div class="pop">\
-                                <div class="content">\
-                                <div class="picker-items-col">\
-                                    <div class="picker-items-col-wrapper">\
-                                        <div class="picker-item">蔡明超</div>\
-                                        <div class="picker-item">王伟</div>\
-                                    </div>\
-                                </div>\
-                                <div class="picker-items-col">\
-                                    <div class="picker-items-col-wrapper">\
-                                        <div class="picker-item">22</div>\
-                                        <div class="picker-item">99</div>\
-                                    </div>\
-                                </div>\
-                                </div>\
-                                <div class="top"></div>\
-                                <div class="bottom"></div>\
-                                </div>'
+                                            <span class="cancel">取消</span>\
+                                            <span class="title">'+this.title+'</span>\
+                                            <span class="confirm">确认</span>\
+                                      </div>\
+                                    <div class="pop">\
+                                        <div class="content">\
+                                            <div class="picker-items-col">\
+                                                <div class="picker-items-col-wrapper">\
+                                                    <div class="picker-item">蔡明超</div>\
+                                                    <div class="picker-item">王伟</div>\
+                                                </div>\
+                                            </div>\
+                                            <div class="picker-items-col">\
+                                                <div class="picker-items-col-wrapper">\
+                                                    <div class="picker-item">22</div>\
+                                                    <div class="picker-item">99</div>\
+                                                </div>\
+                                            </div>\
+                                        </div>\
+                                        <div class="top"></div>\
+                                        <div class="bottom"></div>\
+                                    </div>'
             document.body.appendChild(this.cover)
             document.body.appendChild(this.picker)
             setTimeout(function(){
@@ -67,15 +63,51 @@
                 this.cover.classList.add('show')
             }.bind(this), 20)
             for(var i = 0;i<2;i++){
+                var offsetNum = 0 * this.height; //初始位移的值
                 var el = qsa('.picker-items-col')[i];
-                this._setOffset(el, 0)
-                this._listener(el, 0)
+                this._setOffset(el, i, offsetNum);
+                this._listener(el, i, offsetNum)
             }
         },
         _setOffset(el, i, offsetNum){
-            qs('.picker-items-col-wrapper', el).style.cssText = "transform: translateY("+ 70 +"px)"
+            qs('.picker-items-col-wrapper', el).style.cssText = "transform: translateY("+ (110 - offsetNum) +"px);"
         },
-        _listener(el, i, offsetNum){
+        _listener(el, i, offsetNum){//第三个参数为Y轴的位移
+            var touchStartY = 0, touchMovedY = offsetNum, thisIndex = 0;
+            var lastMoveTime = 0, lastMoveStart = 0, stopInertiaMove = false;
+            function touchstartHandle(e){
+                    touchStartY = e.touches[0].pageY;  //开始触摸的Y轴
+                    thisIndex = i; //第几个col 绑定
+                    lastMoveTime = Date.now(); //最后触摸的事件
+                    lastMoveStart = touchStartY; // 
+            }
+            function touchmoveHandle(e){
+                e.preventDefault();
+                var touchMoveY = e.touches[0].pageY, //移动后的
+                    _touchMovedY = touchStartY - touchMoveY + touchMovedY; //开始的位移
+                this._setOffset(el, thisIndex, _touchMovedY);
+                var nowTime = Date.now();
+                if(nowTime - lastMoveTime > 300){
+                    lastMoveTime = nowTime;
+                    lastMoveStart = touchMoveY; //最后位移记录
+                }
+                
+            }
+            function touchendHandle(e){
+                var toucheEndY = e.changedTouches[0].pageY; //
+                var touchChangedY = touchStartY - toucheEndY + touchMovedY;
+                touchMovedY = touchChangedY;
+                var nowDate = Date.now();
+                
+                
+            }
+            off(el, 'touchstart', touchstartHandle.bind(this))
+            off(el, 'touchmove', touchmoveHandle.bind(this))
+            off(el, 'touchend', touchendHandle.bind(this))
+
+            on(el, 'touchstart', touchstartHandle.bind(this))
+            on(el, 'touchmove', touchmoveHandle.bind(this))
+            on(el, 'touchend', touchendHandle.bind(this));
 
         },
         _event: function(){
@@ -91,9 +123,5 @@
                 this.picker.remove()
             }.bind(this))
         }
-
-
     }
-
-
 })(window)
